80237baf80d9dae154d1eb9e3fd1d70f863a03a1
jin camou and sailor winkelman <sailor@>
2016-09-21 18:24:19 -0700

    Add js to allow user to answer questions 2 and 3. Use branching to return different options.

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,29 @@ $(document).ready(function() {
         $('#snk').show();
       } else {
         $('#question2').show();
-        
       }
   });
+
+  $("form#question2").submit(function(event) {
+    event.preventDefault();
+    var shellQuest = $("input#shell").val();
+      if (shellQuest === "yes") {
+        $('#trt').show();
+      } else {
+        $('#question3').show();
+      }
+  });
+
+  $("form#question3").submit(function(event) {
+    event.preventDefault();
+    var tailQuest = $("input#tail").val();
+      if (tailQuest === "yes") {
+        $('#liz').show();
+      } else {
+        $('#none').show();
+      }
+  });
+
+
+
 });
